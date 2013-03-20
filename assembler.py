524a984a62ecaa12113e02d0b0c3b991ab5c08fc
Harris M. <harrismunir@gmail.com>
2013-03-20 17:06:32 -0500

    Code cleanup, slightly less strict format requirements

--- assembler.py
+++ assembler.py
@@ -1,36 +1,8 @@
-#Y86 Assembler 1.0 by Harris M.
-#Please forgive me for the awful code,
-#I hacked this together really quickly
-#and I'm not a python expert
+#Y86 Assembler 1.1 by Harris M.
 
-#But hey, it works!
+import sys, struct, collections
 
-import sys, struct
-
-#Write the value as one byte, big endian
-def write1(outFile, value):
-	outFile.write(struct.pack(">B", value))
-
-#Write the value as two bytes, big endian
-def write2(outFile, value):
-	outFile.write(struct.pack(">H", value))
-
-#Write the value as four bytes, little endian
-def write4(outFile, value):
-	outFile.write(struct.pack("<i", value))
-
-#Write out a block from the buffer
-def flushBuf(outFile, startAddr, byteCount, outBuf):
-	write2(outFile, startAddr)
-	write2(outFile, byteCount)
-	for i in outBuf:
-		if i[0] == 1:
-			write1(outFile, i[1])
-		elif i[0] == 2:
-			write2(outFile, i[1])
-		elif i[0] == 4:
-			write4(outFile, i[1])
-
+#Register constants
 regs = {
 	'eax': 0,
 	'ecx': 1,
@@ -42,11 +14,65 @@ regs = {
 	'edi': 7
 }
 
+#We'll store our buffered data in this type
+BufferedItem = collections.namedtuple("BufferedVal", ["size", "value"])
+
+#Handles output buffering and writing
+class OutputBuffer(object):
+	def __init__(self, outFile):
+		#A list of tuples to be written out
+		#first value is the number of bytes
+		#second value is the value to write
+		self.buffer = [];
+		self.outFile = outFile;
+		#How many bytes since the last flush
+		self.byteCount = 0;
+		#Starting address to write for the current block
+		self.startAddr = 0;
+
+	#Write the value as one byte, big endian
+	def write1(self, value):
+		self.outFile.write(struct.pack(">B", value))
+
+	#Write the value as two bytes, big endian
+	def write2(self, value):
+		self.outFile.write(struct.pack(">H", value))
+
+	#Write the value as four bytes, little endian
+	def write4(self, value):
+		self.outFile.write(struct.pack("<i", value))
+
+	#Buffer a new item in the block
+	def buf(self, size, value):
+		self.buffer.append(BufferedItem(size, value))
+		self.byteCount += size
+
+	#Write out a block from the buffer
+	def flushBuf(self):
+		self.write2(self.startAddr)
+		self.write2(self.byteCount)
+		for item in self.buffer:
+			if item.size == 1:
+				self.write1(item.value)
+			elif item.size == 2:
+				self.write2(item.value)
+			elif item.size == 4:
+				self.write4(item.value)
+
+	def flushIfBufIsNotEmpty(self):
+		if len(self.buffer) > 0:
+			self.flushBuf();
+
+	def resetBuffer(self, newStartAddr):
+		self.startAddr = newStartAddr
+		self.buffer = []
+		self.byteCount = 0
+
 def main():
 	# Check arguments
 	if len(sys.argv) == 2:
 		inFileName = sys.argv[1]
-		outFileName = "out.yb"
+		outFileName = "out.yb" #Default ouput filename
 	elif len(sys.argv) == 3:
 		inFileName = sys.argv[1]
 		outFileName = sys.argv[2]
@@ -67,140 +93,108 @@ def main():
 		print "Failed to open " + outFileName
 		sys.exit()
 
+	#Create output buffer
+	ob = OutputBuffer(outFile);
+
 	#Write magic number
-	write2(outFile, 0x7962);
+	ob.write2(0x7962);
 
 	#Begin reading file
-	outBuf = [] # A list of tuples that contain the number of bits to write and the value
-	byteCount = 0 # How many bytes have been written since the last flush
-	startAddr = 0 # The address at which this block started
-
 	line = inFile.readline()
 	while line:
-		line = line.strip()
-		if line.startswith(":"):
-			if len(outBuf) > 0:
-				flushBuf(outFile, startAddr, byteCount, outBuf)
-			startAddr = int(line[1:], 16)
-			outBuf = []
-			byteCount = 0
-		else:
-			tokens = line.split(' ')
+		line = line.strip() #Toss extra spaces/newlines
+		tokens = line.split() #Split line on whitespace
+		if line.startswith(":"): #New starting address
+			ob.flushIfBufIsNotEmpty()
+			ob.resetBuffer(int(line[1:], 16))
+		elif len(tokens) > 0:
 			if tokens[0] == "halt":
-				outBuf.append((1, 0x00))
-				byteCount += 1
+				ob.buf(1, 0x00)
 			elif tokens[0] == "nop":
-				outBuf.append((1, 0x10))
-				byteCount += 1
+				ob.buf(1, 0x10)
 			elif tokens[0] == "rrmovl":
-				outBuf.append((1, 0x20))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[2]]))
-				byteCount += 2
+				ob.buf(1, 0x20)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[2]])
 			elif tokens[0] == "irmovl":
-				outBuf.append((1, 0x30))
-				outBuf.append((1, 0xF0 + regs[tokens[2]]))
-				outBuf.append((4, int(tokens[1], 16)))
-				byteCount += 6
+				ob.buf(1, 0x30)
+				ob.buf(1, 0xF0 + regs[tokens[2]])
+				ob.buf(4, int(tokens[1], 16))
 			elif tokens[0] == "rmmovl":
-				outBuf.append((1, 0x40))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[3]]))
-				outBuf.append((4, int(tokens[2], 16)))
-				byteCount += 6
+				ob.buf(1, 0x40)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[3]])
+				ob.buf(4, int(tokens[2], 16))
 			elif tokens[0] == "mrmovl":
-				outBuf.append((1, 0x50))
-				outBuf.append((1, (regs[tokens[3]] << 4) + regs[tokens[2]]))
-				outBuf.append((4, int(tokens[1], 16)))
-				byteCount += 6
+				ob.buf(1, 0x50)
+				ob.buf(1, (regs[tokens[3]] << 4) + regs[tokens[2]])
+				ob.buf(4, int(tokens[1], 16))
 			elif tokens[0] == "add":
-				outBuf.append((1, 0x60))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[2]]));
-				byteCount += 2
+				ob.buf(1, 0x60)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[2]])
 			elif tokens[0] == "sub":
-				outBuf.append((1, 0x61))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[2]]));
-				byteCount += 2
+				ob.buf(1, 0x61)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[2]])
 			elif tokens[0] == "and":
-				outBuf.append((1, 0x62))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[2]]));
-				byteCount += 2
+				ob.buf(1, 0x62)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[2]])
 			elif tokens[0] == "xor":
-				outBuf.append((1, 0x63))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[2]]));
-				byteCount += 2
+				ob.buf(1, 0x63)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[2]])
 			elif tokens[0] == "jmp":
-				outBuf.append((1, 0x70))
-				outBuf.append((4, int(tokens[1], 16)))
-				byteCount += 5
+				ob.buf(1, 0x70)
+				ob.buf(4, int(tokens[1], 16))
 			elif tokens[0] == "jle":
-				outBuf.append((1, 0x71))
-				outBuf.append((4, int(tokens[1], 16)))
-				byteCount += 5
+				ob.buf(1, 0x71)
+				ob.buf(4, int(tokens[1], 16))
 			elif tokens[0] == "jl":
-				outBuf.append((1, 0x72))
-				outBuf.append((4, int(tokens[1], 16)))
-				byteCount += 5
+				ob.buf(1, 0x72)
+				ob.buf(4, int(tokens[1], 16))
 			elif tokens[0] == "je":
-				outBuf.append((1, 0x73))
-				outBuf.append((4, int(tokens[1], 16)))
-				byteCount += 5
+				ob.buf(1, 0x73)
+				ob.buf(4, int(tokens[1], 16))
 			elif tokens[0] == "jne":
-				outBuf.append((1, 0x74))
-				outBuf.append((4, int(tokens[1], 16)))
-				byteCount += 5
+				ob.buf(1, 0x74)
+				ob.buf(4, int(tokens[1], 16))
 			elif tokens[0] == "jge":
-				outBuf.append((1, 0x75))
-				outBuf.append((4, int(tokens[1], 16)))
-				byteCount += 5
+				ob.buf(1, 0x75)
+				ob.buf(4, int(tokens[1], 16))
 			elif tokens[0] == "jg":
-				outBuf.append((1, 0x76))
-				outBuf.append((4, int(tokens[1], 16)))
-				byteCount += 5
+				ob.buf(1, 0x76)
+				ob.buf(4, int(tokens[1], 16))
 			elif tokens[0] == "cmovle":
-				outBuf.append((1, 0x21))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[2]]))
-				byteCount += 2
+				ob.buf(1, 0x21)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[2]])
 			elif tokens[0] == "cmovl":
-				outBuf.append((1, 0x22))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[2]]))
-				byteCount += 2
+				ob.buf(1, 0x22)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[2]])
 			elif tokens[0] == "cmove":
-				outBuf.append((1, 0x23))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[2]]))
-				byteCount += 2
+				ob.buf(1, 0x23)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[2]])
 			elif tokens[0] == "cmovne":
-				outBuf.append((1, 0x24))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[2]]))
-				byteCount += 2
+				ob.buf(1, 0x24)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[2]])
 			elif tokens[0] == "cmovge":
-				outBuf.append((1, 0x25))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[2]]))
-				byteCount += 2
+				ob.buf(1, 0x25)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[2]])
 			elif tokens[0] == "cmovg":
-				outBuf.append((1, 0x26))
-				outBuf.append((1, (regs[tokens[1]] << 4) + regs[tokens[2]]))
-				byteCount += 2
+				ob.buf(1, 0x26)
+				ob.buf(1, (regs[tokens[1]] << 4) + regs[tokens[2]])
 			elif tokens[0] == "call":
-				outBuf.append((1, 0x80))
-				outBuf.append((4, int(tokens[1], 16)))
-				byteCount += 5
+				ob.buf(1, 0x80)
+				ob.buf(4, int(tokens[1], 16))
 			elif tokens[0] == "ret":
-				outBuf.append((1, 0x90))
-				byteCount += 1
+				ob.buf(1, 0x90)
 			elif tokens[0] == "push":
-				outBuf.append((1, 0xA0))
-				outBuf.append((1, (regs[tokens[1]] << 4) + 0xF))
-				byteCount += 2
+				ob.buf(1, 0xA0)
+				ob.buf(1, (regs[tokens[1]] << 4) + 0xF)
 			elif tokens[0] == "pop":
-				outBuf.append((1, 0xB0))
-				outBuf.append((1, (regs[tokens[1]] << 4) + 0xF))
-				byteCount += 2
+				ob.buf(1, 0xB0)
+				ob.buf(1, (regs[tokens[1]] << 4) + 0xF)
 
-		line = inFile.readline()
+		line = inFile.readline() #grab the next line
 
-	if len(outBuf) > 0:
-		flushBuf(outFile, startAddr, byteCount, outBuf)
+	ob.flushIfBufIsNotEmpty() #write out the last block
 
-
+	#Cleanup
 	inFile.close()
 	outFile.close()
 
